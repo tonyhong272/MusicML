e2413deead241bbb3887a0a7db15d5707da1d834
tonyhong272 <tonyhong272@gmail.com>
2016-02-27 23:56:23 -0800

    scraping bb complete

--- DataScraping/fetchBBdata.py
+++ DataScraping/fetchBBdata.py
@@ -4,11 +4,15 @@ Created on Sun Feb 21 16:53:47 2016
 
 @author: XH
 """
-
+#%%
 import billboard
 import sqlite3 as lite
-
-
+import time
+#%%
+scrapeDepth = 10 #number of charts to scrape
+startingDate = None
+#startingDate = '1984-01-28'
+#%%
 def insertChart2DB (chart):
     with con:
         cur = con.cursor()    
@@ -17,9 +21,44 @@ def insertChart2DB (chart):
         for entry in chart:
             ID = chart.date + '-' + str(entry.rank)
             single_entry = [(ID, chart.date, entry.title, entry.artist, entry.rank, entry.peakPos, entry.lastPos, entry.weeks,entry.change, entry.spotifyLink)]
-            cur.executemany('INSERT INTO SongEntries VALUES (?,?,?,?,?,?,?,?,?,?)', single_entry)
+            cur.executemany('INSERT OR IGNORE INTO SongEntries VALUES (?,?,?,?,?,?,?,?,?,?)', single_entry)
+            
+def fetchDatesInDB():
+    with con:
+        cur = con.cursor()
+        cur.execute('SELECT DISTINCT BBDate FROM SongEntries')
+    return cur
+    
+def chartIsInDB (chart):
+    availableDates = fetchDatesInDB()     
+    return chart.date in availableDates
+    
+def scrapingChart(date):
+    chart = billboard.ChartData('hot-100', date=date, fetch=True)
+    print ('Now Scraping '+chart.date)
+    time.sleep(0.1)
+    return chart
 
-chart = billboard.ChartData('hot-100', date=None, fetch=True)
+#%%
 con = lite.connect('billboardDB.db')
-insertChart2DB(chart)
+
+chart = scrapingChart(date = startingDate)
+if not chartIsInDB(chart):
+    insertChart2DB(chart)
+
+for indx in range(scrapeDepth):
+    chart = scrapingChart(chart.previousDate)
+    if not chartIsInDB(chart):
+        insertChart2DB(chart)
+    
 con.close()
+
+
+
+#==============================================================================
+# SELECT DISTINCT BBDate FROM SongEntries
+# WHERE DATE(substr(BBDate,1,4)
+# ||substr(BBDate,6,2)
+# ||substr(BBDate,9,2)) 
+# BETWEEN DATE(20131020) AND DATE(20161021);
+#==============================================================================
