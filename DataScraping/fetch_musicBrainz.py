e6c3db49d21d4a41beca1d569f484112bb74e281
tonyhong272 <tonyhong272@gmail.com>
2016-04-12 23:09:45 -0700

    fetch musicbrainz into database, bug fixes, matching solved

--- DataScraping/fetch_musicBrainz.py
+++ DataScraping/fetch_musicBrainz.py
@@ -12,14 +12,14 @@ def form_track(song):
     track={}
     track['artist'] = song[1]
     track['title'] = song[0]
-    if track['artist'].find('Featuring') != -1:
+    '''if track['artist'].find('Featuring') != -1:
         track['artist'] = track['artist'][0:track['artist'].find('Featuring')-1]
     if track['artist'].find('With') != -1:
         track['artist'] = track['artist'][0:track['artist'].find('With')-1]
     if track['artist'].find('&') != -1:
         track['artist'] = track['artist'][0:track['artist'].find('&')-1]
     if track['artist'].find('/') != -1:
-        track['artist'] = track['artist'][0:track['artist'].find('/')-1]
+        track['artist'] = track['artist'][0:track['artist'].find('/')-1]'''
     return track
 
 def fetch_infos(track):
@@ -34,12 +34,13 @@ def save_sqlite(track_info):
     con = lite.connect('mbInfo.db')
     with con:
         cur = con.cursor()
-        cur.execute("CREATE TABLE IF NOT EXISTS TrackEntries(ID INTEGER PRIMARY KEY, title TEXT, artist TEXT, recording_length TEXT, \
+        cur.execute("CREATE TABLE IF NOT EXISTS TrackEntries(ID INTEGER PRIMARY KEY, title TEXT, artist TEXT,\
+                    mb_artist TEXT, mb_title TEXT, match_score REAL, recording_length TEXT, \
                     recording_mbid TEXT, artist_mbid TEXT, release_date TEXT, release_mbid TEXT,artist_type TEXT, \
                     artist_gender TEXT, artist_begin_area TEXT, artist_begin_date TEXT, artist_country TEXT, artist_tags TEXT)")
         single_entry = tuple(track_info.values())
-        print ','.join(track_info.keys())
-        cur.execute('INSERT OR IGNORE INTO TrackEntries(ID,' + ','.join(track_info.keys()) + ') VALUES (NULL,?,?,?,?,?,?,?,?,?,?,?,?,?)', single_entry)
+        #print ','.join(track_info.keys())
+        cur.execute('INSERT OR IGNORE INTO TrackEntries(ID,' + ','.join(track_info.keys()) + ') VALUES (NULL,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)', single_entry)
     con.close()
 
 def fetch_BBSong_list():
@@ -58,4 +59,4 @@ if __name__ == '__main__':
         count += 1
         track_info = fetch_infos(track)
         save_sqlite(track_info)
-        print count, str(track)
+        print count, track, track_info['mb_title'], track_info['mb_artist']
--- DataScraping/musicBrainz/recordingSearch.py
+++ DataScraping/musicBrainz/recordingSearch.py
@@ -9,6 +9,7 @@ database and prints out a few details about the first 5 matching release.
 """
 import musicbrainzngs
 import sys
+import difflib
 
 musicbrainzngs.set_useragent(
     "python-musicbrainzngs-example",
@@ -18,24 +19,44 @@ musicbrainzngs.set_useragent(
 
 musicbrainzngs.set_hostname("localhost:5000")
 
+def similar(seq1, seq2):
+    return difflib.SequenceMatcher(a=seq1.lower(), b=seq2.lower()).ratio()
 
 def fetch_recording(artist, title, **kwarg):
     """fetch recording from musicBrainz
     """
-    result = musicbrainzngs.search_recordings(query='', limit=5, offset=None, strict=False, artist = artist, release = title)
-    recording = {};
-    for i in range(0,5):
-        if (not 'disambiguation' in result['recording-list'][i]) or \
-        (result['recording-list'][i]['disambiguation'] != 'music video'):
-            if 'length' in result['recording-list'][0]:
-                recording['recording_length'] = result['recording-list'][0]['length']
-            else:
-                recording['recording_length'] = -1
-            recording['recording_mbid'] = result['recording-list'][0]['id']
-            recording['artist_mbid'] = result['recording-list'][0]['artist-credit'][0]['artist']['id']
-            recording['release_date'] = result['recording-list'][0]['release-list'][0]['date']
-            recording['release_mbid'] = result['recording-list'][0]['release-list'][0]['id']
-            return recording
+    result = musicbrainzngs.search_recordings(query='', limit=10, offset=None, strict=False, artist = artist, release = title)
+    seq2 =' '.join([title, artist])
+    high_score = 0
+    idx = 0
+    for i in range(0,10):
+        seq1 = ' '.join([result['recording-list'][i]['title'], result['recording-list'][i]['artist-credit-phrase']])
+        similarity_score = similar(seq1,seq2)
+        if similarity_score > high_score and 'instrumental' not in seq1 and (not 'disambiguation' in result['recording-list'][i] or \
+        (result['recording-list'][i]['disambiguation'] != 'music video')):
+            high_score = similarity_score
+            idx = i
+            '''print similarity_score
+            print seq1
+            print seq2'''
+    #print idx
+    recording = {}
+    recording['match_score'] = high_score
+    if 'length' in result['recording-list'][idx]:
+        recording['recording_length'] = result['recording-list'][idx]['length']
+    else:
+        recording['recording_length'] = -1
+    recording['recording_mbid'] = result['recording-list'][idx]['id']
+    recording['artist_mbid'] = result['recording-list'][idx]['artist-credit'][0]['artist']['id']
+    recording['release_date'] = result['recording-list'][idx]['release-list'][0]['date']
+    recording['release_mbid'] = result['recording-list'][idx]['release-list'][0]['id']
+    if 'artist-credit-phrase' in result['recording-list'][idx]:
+        recording['mb_artist'] = result['recording-list'][idx]['artist-credit-phrase']
+    else:
+        recording['mb_artist'] = ''
+    recording['mb_title'] = result['recording-list'][idx]['title']
+    #print result['recording-list']
+    return recording
 
 def fetch_artist(artist_mbid, **kwarg):
     """fetch artist info from musicBrainz
@@ -43,7 +64,10 @@ def fetch_artist(artist_mbid, **kwarg):
     result = musicbrainzngs.search_artists(query='', limit=1, offset=None, strict=False, arid = artist_mbid)
     artist_info = {};
     if 'artist-list' in result:
-        artist_info['artist_type'] = result['artist-list'][0]['type']
+        if 'type' in result['artist-list'][0]:
+            artist_info['artist_type'] = result['artist-list'][0]['type']
+        else:
+            artist_info['artist_type'] = ''
         if 'gender' in result['artist-list'][0]:
             artist_info['artist_gender'] = result['artist-list'][0]['gender']
         else:
@@ -51,15 +75,15 @@ def fetch_artist(artist_mbid, **kwarg):
         if 'begin-area' in result['artist-list'][0]:
             artist_info['artist_begin_area'] = result['artist-list'][0]['begin-area']['name']
         else:
-            artist_info['artist_begin_area-area'] = ''
+            artist_info['artist_begin_area'] = ''
         if 'country' in result['artist-list'][0]:
             artist_info['artist_country'] = result['artist-list'][0]['country']
         else:
             artist_info['artist_country'] = ''
-        if 'life-span' in result['artist-list'][0]:
+        if 'life-span' in result['artist-list'][0] and 'begin' in result['artist-list'][0]['life-span']:
             artist_info['artist_begin_date'] = result['artist-list'][0]['life-span']['begin']
         else:
-            artist_info['artist_begin_date-date'] = ''
+            artist_info['artist_begin_date'] = ''
         if 'tag-list' in result['artist-list'][0]:
             tag_list = result['artist-list'][0]['tag-list']
             artist_info['artist_tags'] = ';'.join([tag['name'] for tag in tag_list])
@@ -68,7 +92,7 @@ def fetch_artist(artist_mbid, **kwarg):
     return artist_info
 
 if __name__ == '__main__':
-    args = ['Rihanna', 'Work']
+    args = ['Rihanna', 'umbrella']
     print(args)
     if len(args) != 2:
         sys.exit("usage: {} ARTIST ALBUM".format(sys.argv[0]))
@@ -76,6 +100,6 @@ if __name__ == '__main__':
 
     recording = fetch_recording(artist, title)
     artist = fetch_artist(recording['artist_mbid'])
-    print recording, artist 
+    print recording, artist
 
 
